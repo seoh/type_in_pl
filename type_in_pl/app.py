d7cae7f60bfe566b0fff135d57572981160bd683
seoh <devthewild@gmail.com>
2022-07-21 01:42:25 +0900

    read toc and extract titles and links
    div.article_view: str -> List[(List[str], str)]
    it will be nested dict or json like this.
    - intro
    - ch01
      - ch01.1
      - ch02.2
    - ch02
    ...
    - outro

--- type_in_pl/app.py
+++ type_in_pl/app.py
@@ -1,8 +1,30 @@
+from functools import reduce
 import requests
 from bs4 import BeautifulSoup
 
-INTRO = "https://blog.hjaem.info/2"
+BASE = "https://blog.hjaem.info/"
+TOC  = BASE + "2"
 
 def main():
-  res = BeautifulSoup(requests.get(INTRO).content, 'html.parser')
-  print(res.prettify())
+  res = BeautifulSoup(requests.get(TOC).content, 'html.parser')
+  article = res.select_one('.article_view')
+
+  intro = article.select_one('p > a[href^="{}"]'.format(BASE))
+  outro = article.select('p > a[href^="{}"]'.format(BASE))[-1]
+
+  from itertools import takewhile, dropwhile
+  ps = article.select('p')
+  lpad = dropwhile(lambda p: p != intro.parent, ps)
+  rpad = takewhile(lambda p: p != outro.parent, lpad)
+
+  ps = list(rpad)
+  ps.append(outro.parent)
+
+  paired = list(map(
+    lambda p: (
+      list(map(lambda c: c.text.replace('\xa0', ' '), p.contents)), # title: List[str]
+      p.contents[-1].attrs.get('href')),                            # href: str | None
+    ps))                                                            # -> List[(List[str], str | None)]
+
+  for p in paired:
+    print(p)
